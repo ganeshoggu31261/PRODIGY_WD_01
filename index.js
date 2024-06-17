ab3ef89c613517fb789329b30df39e9b83aebf8f
ganeshoggu31261 <2200031261@kluniversity.in>
2024-06-17 15:25:44 +0530

    commit 1

---
+++ index.js
@@ -0,0 +1,14 @@
+function  changeBg(){
+    var navbar=document.getElementById('navbar');
+    var scrollValue=window.scrollY;
+    if(scrollValue <150){
+        navbar.classList.remove('bgcolor');
+
+    }
+    else{
+        navbar.classList.add('bgcolor');
+    }
+    
+
+}
+window.addEventListener('scroll',changeBg);
